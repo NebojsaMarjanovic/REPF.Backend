8f57aeb2ac5aa1cad3c7d4ccebcc9393d5bb54ce
nebojsa.marjanovic <nebojsa.marjanovic@teletrader.com>
2023-04-25 10:30:39 +0200

    Added models and data

---
+++ REPF.Grpc/Models/RealEstate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.ML.Data;
+using System;
+
+namespace REPF.Grpc.Models
+{
+    public class RealEstate
+    {
+        [LoadColumn(0)]
+        public int m2 { get; set; }
+        [LoadColumn(1)]
+        public string heatingType { get; set; } //district-centralno; central-etazno
+        [LoadColumn(2)]
+        public int elevator { get; set; }
+        [LoadColumn(3)]
+        public double price { get; set; }
+        [LoadColumn(4)]
+        public DateTime createdAt { get; set; }
+        [LoadColumn(5)]
+        public double roomCount { get; set; }
+        [LoadColumn(6)]
+        public int redactedFloor { get; set; }
+        [LoadColumn(7)]
+        public string placeTitle { get; set; }
+    }
+}
