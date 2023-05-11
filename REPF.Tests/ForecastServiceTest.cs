a4f3b6cdcf0df49c13acb411a7a47159c45ca246
nebojsa.marjanovic <nebojsa.marjanovic@teletrader.com>
2023-05-11 14:18:49 +0200

    Added some unit tests

---
+++ REPF.Tests/ForecastServiceTest.cs
@@ -0,0 +1,56 @@
+using FluentAssertions;
+using Grpc.Core;
+using REPF.Grpc;
+using REPF.Grpc.Models;
+using REPF.Grpc.Services;
+
+namespace REPF.Tests
+{
+    public class ForecastServiceTest
+    {
+       
+        RealEstate request = new RealEstate()
+        {
+            Location = "Kanarevo Brdo",
+            Quadrature = 78,
+            RoomCount = 3,
+            RedactedFloor = 4,
+            IsLastFloor = false,
+            RegisteredStatus = "yes",
+            FurnishedStatus = "no",
+            HeatingType = "district",
+            Elevator = 0,
+            Price = 0
+        };
+
+        ForecastService sut = new ForecastService();
+
+
+        [Fact]
+        public void EvaluateModel_ShouldHaveAcceptableMetrics()
+        {
+            var model = sut.Train();
+            var metrics = sut.Evaluate(model);
+
+            metrics.RSquared.Should().BeGreaterThanOrEqualTo(0.85);
+        }
+
+        [Fact]
+        public void PredictSinglePrediction_ShouldReturnPredictedPrice()
+        {
+            var model = sut.Train();
+
+            var singlePrediction = sut.TestSinglePrediction(model, request);
+
+            singlePrediction.Price.Should().BeGreaterThan(0);
+        }
+
+        [Fact] 
+        public void TrainModel_ShouldReturnTrainedModel()
+        {
+            var model = sut.Train();
+            model.Should().NotBeNull();
+        }
+
+    }
+}
